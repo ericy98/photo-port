df2569c3def38a2a77180a8c845ac3bf03c80af1
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-03 13:56:50 -0500

    add submit and change handlers:

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    
+    // destructure object into named properties
+    const { name, email, message } = formState; 
+
+    function handleChange(e) {
+        // spread op; sync form data w/ dynamic property names
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" onChange={handleChange} defaultValue={name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" onChange={handleChange} defaultValue={email} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5"  onChange={handleChange} defaultValue={message} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
